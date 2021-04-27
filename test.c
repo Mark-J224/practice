55765f8dbd5674d29d1247ce0e3ef0b1ff16f48c
Mark Jaschke <mjaschke@colgate.edu>
2021-04-27 00:48:42 +0000

    new tests

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
 #include <stdio.h> 
+#include <pthread.h> 
 #include <string.h> 
 #include <sys/types.h> 
 #include <sys/wait.h> 
 #include <unistd.h>
+#include <stdio.h>
+
 
  int main() { 
     printf("Hello World \n");
@@ -30,9 +33,5 @@
         wait(0); 
         printf("This is the parent process: %d \n", getpid()); 
     }
-
-
-
-
 
  }
